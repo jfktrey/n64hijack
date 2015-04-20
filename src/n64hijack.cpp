7d09b7b88b8f89467cc5c27ab4aab13b62e8a352
treykeown <jfktrey@gmail.com>
2015-04-20 15:08:36 -0500

    Fix command-line argument detection
    Error wasn’t displayed when there are less than 3 parameters passed
    (minimum). Now fixed.

--- src/n64hijack.cpp
+++ src/n64hijack.cpp
@@ -219,7 +219,7 @@ int main (int argc, char** argv) {
 
     if (argc == 5) {
         if (strcmp(argv[4], "--noWatchKill")) argumentError();
-    } else if (argc > 5) {
+    } else if ((argc > 5) || (argc < 4)) {
         argumentError();
     }
 
